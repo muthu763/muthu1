3dee70a97b2df3f4392a99ff77695a5cbd9873b7
muthu763 <magizhchi8@gmail.com>
2019-11-22 12:08:54 +0530

    new code joining

---
+++ src/org/s/A.java
@@ -0,0 +1,16 @@
+package org.s;
+
+public class A {
+	public void empid() {
+
+System.out.println("aravind");
+
+	}
+	public static void main(String[] args) {
+		A e=new A();
+		e.empid();
+	}
+	
+
+
+}
---
+++ src/org/s/Ar.java
@@ -0,0 +1,10 @@
+package org.s;
+
+public class Ar {
+public void empid() {
+System.out.println("aaa");
+}
+public void national() {
+	System.out.println("bbb");
+}
+}
