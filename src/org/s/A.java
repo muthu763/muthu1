300408b627f58aa84dc08bc9c61b6bf37c903ac9
muthu763 <magizhchi8@gmail.com>
2019-11-27 14:01:24 +0530

    second code

--- src/org/s/A.java
+++ src/org/s/A.java
@@ -6,9 +6,10 @@ public class A {
 System.out.println("aravind");
 
 	}
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Throwable {
 		A e=new A();
 		e.empid();
+		Thread.sleep(2000);
 	}
 	
 
